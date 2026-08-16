e4f9aacb7662088f4a81503c7b07c7bce8097383
M ANBU MARISH <113194705+marish2001@users.noreply.github.com>
2026-08-16 03:28:29 -0700

    Update script.js

--- Weatherappproject/script.js
+++ Weatherappproject/script.js
@@ -1,10 +1,36 @@
-const { response } = require("express")
+const apikey = "7b3ff95fe002c8ee353ff0bc08d2e34b"
 
-async function apidetails()
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+
+button.onclick = async()=>
 {
-   const api_response = await fetch("https://dog.ceo/api/breeds/image/random")
-   const data = await response.json()
-   console.log(data)
-}
+    let city_name = input.value.trim()
 
-apidetails()
+    if(city_name==="")
+    {
+        alert("please enter the city name correctly")
+        return;
+    }
+
+    try 
+    {
+        let apiurl = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=${apikey}`)
+        let response = await apiurl.json()
+         
+        if(response.cod!=200)
+        {
+            alert("You are not enter the city name")
+            return;
+        }
+
+        document.querySelector(".city").textContent = response.name;
+        document.querySelector(".temperature").textContent = Math.round(response.main.temp) +"°C";
+        document.querySelector(".description").textContent = response.weather[0].description;
+    }
+
+    catch(error)
+    {
+        console.error("API details not fetched correctly")
+    }
+}
